d37d391c304463fc796428279575065e71d62641
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-17 04:01:24 +0200

    finish video 3 and 4 types of views and types of action return type and how we can write c# inside html

--- GymManagementPL/Controllers/HomeController.cs
+++ GymManagementPL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
 {
@@ -6,9 +7,38 @@ namespace GymManagementPL.Controllers
     {
         //Actions
         //BaseUrl/Home/Index    
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
+
+        public JsonResult Trainers()
+        {
+            var Trainers = new List<Trainer>()
+            {
+                new Trainer() { Name = "Ahmed", Phone = "011254696" },
+                new Trainer() { Name = "Aya", Phone = "01127777" }
+            };
+            return Json(Trainers);
+        }
+        public ContentResult Content()
+        {
+            return Content("<h1>Hello From Gym Management System</h1>", "text/html");
+        }
+
+        public FileResult Downloadfile()
+        {
+            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
+            var FileBytes = System.IO.File.ReadAllBytes(FilePath);
+
+            return File(FileBytes, "text/css", "DownloadSite.css");
+        }
+
+        public EmptyResult EmptyAction()
+        {
+            return new EmptyResult();
+        }
+
+
     }
 }
