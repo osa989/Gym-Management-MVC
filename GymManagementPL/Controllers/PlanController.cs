647e986a501e5f0135f3cdf1022f8a271a2068ec
osa989 <osamagamal9988@gmail.com>
2025-12-29 16:58:40 +0200

    case I can face if edit and details are the same

--- GymManagementPL/Controllers/PlanController.cs
+++ GymManagementPL/Controllers/PlanController.cs
@@ -17,7 +17,7 @@ namespace GymManagementPL.Controllers
             return View(Plans);
         }
         
-        public ActionResult Details([FromRoute]int id)
+        public ActionResult Details([FromRoute]int id, string ViewName ="Details")
         {
             if(id<= 0)
             {
@@ -30,7 +30,42 @@ namespace GymManagementPL.Controllers
                 TempData["ErrorMessage"] = "Plan not found ";
                 return RedirectToAction(nameof(Index));
             }
-            return View(Plan);
+            return View(ViewName,Plan);
         }
-    }
+        public ActionResult Edit([FromRoute] int id)
+        {
+            //if (id <= 0)
+            //{
+            //    TempData["ErrorMessage"] = "Invalid Plan Id ";
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //var PlanToUpdate = _planService.GetPlanToUpdate(id);
+            //if (PlanToUpdate == null)
+            //{
+            //    TempData["ErrorMessage"] = "Plan not found or cannot be updated ";
+            //    return RedirectToAction(nameof(Index));
+            //}
+            return Details(id,"Edit");
+        }
+        [HttpPost]
+        public ActionResult Edit([FromRoute] int id, UpdatePlanViewModel UpdatedPlan)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid Plan Id ";
+                return RedirectToAction(nameof(Index));
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(UpdatedPlan);
+            }
+            var isUpdated = _planService.UpdatePlan(id, UpdatedPlan);
+            if (!isUpdated)
+            {
+                TempData["ErrorMessage"] = "Plan not found or cannot be updated ";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["SuccessMessage"] = "Plan updated successfully ";
+            return RedirectToAction(nameof(Index));
+        }
 }
